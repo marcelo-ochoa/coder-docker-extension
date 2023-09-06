d1dbb3f4d82e3a35f3fd933b6ac89cb55a2ef89d
Marcelo Ochoa <mochoa.ext@andromedalatam.com>
2023-09-06 16:04:39 -0300

    added extra deps

--- addPython.sh
+++ addPython.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+apt-get update
+apt install -y python3-pip python3-venv
 su -l coder /bin/sh -c "code-server --force --install-extension=ms-python.python"
